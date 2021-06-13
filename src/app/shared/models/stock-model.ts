4546323e886e35393bee519f2bdb3be47c153209
Lúbia Oliveira <lubia.oliveira01@gmail.com>
2021-06-13 14:05:19 -0300

    Integração com a API

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,13 @@
+class Stock {
+  id: number;
+  name: string;
+  price: number;
+  minium: number;
+  exclusiveMinium: boolean;
+  variation: number;
+  date: string;
+  /*list: string[]; -> tipo de array*/
+}
+
+export default Stock;
+
